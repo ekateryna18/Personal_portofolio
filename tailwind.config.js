132d3a5fce41279c7428a6f76b5d74e15c7af9d7
Munteanu Ecaterina <munteanu_ecaterina@yahoo.com>
2025-05-31 14:37:34 +0300

    added project front

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,30 @@
 module.exports = {
     plugins: [
-      require('taos/plugin')
+      require('taos/plugin'),
+      require('tailwind-scrollbar-hide')
     ],
-  }
-module.exports = {
     safelist: [
-        '!duration-[0ms]',
-        '!delay-[0ms]',
-        'html.js :where([class*="taos:"]:not(.taos-init))'
-    ]
-}
-module.exports = {
-    content: {
-      relative: true,
-      transform: (content) => content.replace(/taos:/g, ''),
-      files: ['./src/*.{html,js}'],
-    },
+      '!duration-[0ms]',
+      '!delay-[0ms]',
+      'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['./src/*.{html,js}'],
   }
+  }
+// module.exports = {
+//     safelist: [
+//         '!duration-[0ms]',
+//         '!delay-[0ms]',
+//         'html.js :where([class*="taos:"]:not(.taos-init))'
+//     ]
+// }
+// module.exports = {
+//     content: {
+//       relative: true,
+//       transform: (content) => content.replace(/taos:/g, ''),
+//       files: ['./src/*.{html,js}'],
+//     },
+//   }
